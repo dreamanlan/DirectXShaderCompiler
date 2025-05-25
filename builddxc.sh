7a46a280a414bb36de8fd1a96a9d628ae5ffbc84
dreamanlan <dreaman_163@163.com>
2025-05-26 00:31:18 +0800

    build on mac

--- builddxc.sh
+++ builddxc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 mkdir -p dxcbuild
+
 cd dxcbuild
 
-cmake ../DirectXShaderCompiler -GNinja -DCMAKE_BUILD_TYPE=Release -C ../DirectXShaderCompiler/cmake/caches/PredefinedParams.cmake -DCMAKE_C_COMPILER=clang
+#can't genenerate xcode project for SPIRV-Tools
+#cmake ../ -B . -GXcode -DCMAKE_BUILD_TYPE=Release -C ../cmake/caches/PredefinedParams.cmake -DCMAKE_C_COMPILER=clang
+
+cmake ../ -B . -GNinja -DCMAKE_BUILD_TYPE=Release -C ../cmake/caches/PredefinedParams.cmake -DCMAKE_C_COMPILER=clang
+
 ninja
 

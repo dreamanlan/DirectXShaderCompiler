29539b7242a10e32a010c7e25e0589e9548811da
dreamanlan <dreaman_163@163.com>
2025-06-15 22:35:39 +0800

    Update builddxc.sh

--- builddxc.sh
+++ builddxc.sh
@@ -7,7 +7,8 @@ cd dxcbuild
 #can't genenerate xcode project for SPIRV-Tools
 #cmake ../ -B . -GXcode -DCMAKE_BUILD_TYPE=Release -C ../cmake/caches/PredefinedParams.cmake -DCMAKE_C_COMPILER=clang
 
-cmake ../ -B . -GNinja -DCMAKE_BUILD_TYPE=Release -C ../cmake/caches/PredefinedParams.cmake -DCMAKE_C_COMPILER=clang
+cmake ../ -B . -GNinja -DCMAKE_BUILD_TYPE=Release -C ../cmake/caches/PredefinedParams.cmake -DCMAKE_C_COMPILER=clang -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
 
 ninja
 
+cd ..
